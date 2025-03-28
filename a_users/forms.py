2762a72eca21ee39f8c46db81d96dd715d470f14
andrehsilva <andrehrsilva@gmail.com>
2025-03-28 08:26:17 -0300

    alterações no profile + cpf +email

--- a_users/forms.py
+++ a_users/forms.py
@@ -6,14 +6,25 @@ from .models import Profile
 class ProfileForm(ModelForm):
     class Meta:
         model = Profile
-        fields = ['image', 'displayname', 'info' ]
+        fields = ['image', 'displayname', 'info', 'phone','cpf' ]
         widgets = {
             'image': forms.FileInput(),
             'displayname' : forms.TextInput(attrs={'placeholder': 'Add display name'}),
+            'cpf': forms.TextInput(attrs={'placeholder': 'Enter your CPF'}),
+            'phone': forms.TextInput(attrs={'placeholder': 'Enter your phone number'}),
             'info' : forms.Textarea(attrs={'rows':3, 'placeholder': 'Add information'})
         }
         
-        
+    # Validação personalizada para o CPF, caso precise de algo adicional
+    def clean_cpf(self):
+        cpf = self.cleaned_data.get('cpf')
+        # Se houver algum validador ou lógica adicional de CPF, adicione aqui
+        # Exemplo:
+        if len(cpf) != 11:  # Apenas exemplo, ajuste conforme sua validação real
+            raise forms.ValidationError("CPF must have 11 digits")
+        return cpf
+    
+
 class EmailForm(ModelForm):
     email = forms.EmailField(required=True)
 
--- a_users/models.py
+++ a_users/models.py
@@ -1,11 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.core.exceptions import ValidationError
 from django.conf import settings
+import re
+
+# Validador de CPF
+def validate_cpf(value):
+    if not re.match(r'^\d{11}$', value):
+        raise ValidationError("CPF deve ter 11 dígitos numéricos.")
+
+
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     image = models.ImageField(upload_to='avatars/', null=True, blank=True)
     displayname = models.CharField(max_length=20, null=True, blank=True)
+    cpf = models.CharField(max_length=11, unique=True, validators=[validate_cpf])
+    phone = models.CharField(max_length=20, blank=True, null=True)
     info = models.TextField(null=True, blank=True) 
     
     def __str__(self):
@@ -22,3 +34,4 @@ class Profile(models.Model):
         if self.image:
             return self.image.url
         return f'{settings.STATIC_URL}images/avatar.svg'
+
