beddf0c35d310d932270261be7eb86103de1bea9
andrehsilva <andrehrsilva@gmail.com>
2025-06-23 14:18:40 -0300

    allowed

--- a_core/settings.py
+++ a_core/settings.py
@@ -34,9 +34,9 @@ if ENVIRONMENT == 'development':
 else:
     DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'saasschool-production.up.railway.app']
 
-CSRF_TRUSTED_ORIGINS = [ 'https://*' ]
+CSRF_TRUSTED_ORIGINS = [ 'https://saasschool-production.up.railway.app' ]
 
 #ENVIRONMENT = "development"  # Ou "production"
 
